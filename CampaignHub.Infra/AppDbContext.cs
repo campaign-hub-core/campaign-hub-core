41d2f93fc99bfd758c8e3868a629753e18d1c35c
gabriel ferreira <gabrielferreiracunha@gmail.com>
2026-02-02 23:48:09 -0300

    feat: Add AppContext

---
+++ CampaignHub.Infra/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CampaignHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampaignHub.Infra;
+
+public class AppDbContext : DbContext
+{
+    protected AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Organization> Organizations { get; set; }
+    public DbSet<MetricCampaign> MetricCampaigns { get; set; }
+    public DbSet<Campaign> Campaigns { get; set; }
+    public DbSet<AdAccount> AdAccounts { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
